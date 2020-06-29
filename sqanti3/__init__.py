ae93ff4eb8d1ba93a33be0c366a83910fcbb3189
Miles Smith <mileschristiansmith@gmail.com>
2020-06-29 18:03:55 +0000

    :another __version__ fix attempt

--- sqanti3/__init__.py
+++ sqanti3/__init__.py
@@ -1,1 +1,9 @@
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
 __author__ = __email__ = ("fraparp1@upv.edu.es", "pedsalga@upv.edu.es")
