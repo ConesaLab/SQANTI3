5fa99356e51eb0c0b1b4dccee86538c9adfedd8b
psalguerog <pedsalga@upv.edu.es>
2021-04-20 15:46:09 +0200

    Fixed IsoAnnotLite version name

--- utilities/IsoAnnotLite_SQ3.py
+++ utilities/IsoAnnotLite_SQ3.py
@@ -15,7 +15,7 @@ ALL_AS_NOVELS = False
 INTRONIC = True
 STATS = False
 
-version = "2.7"
+version = "2.6"
 CLASS_COLUMN_USED = [0,1,2,3,5,6,7,30,32,33]
 CLASS_COLUMN_NAME = ["isoform", "chrom", "strand", "length", "structural_category", "associated_gene", "associated_transcript", "ORF_length","CDS_start", "CDS_end"]
 LST_TRANSCRIPTFEATURES_NOTIN_CDS = ["uORF", "miRNA_Binding", "PAS", "3UTRmotif", "5UTRmotif"]
