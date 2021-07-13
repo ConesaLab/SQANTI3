24d8717609b0ac98a3b88c8bd3d88bb73b5385b7
Angeles <angeles.arzalluz@gmail.com>
2021-07-13 18:22:07 +0200

    Add script info, args, input loading and plot theme to MLreport

--- utilities/SQANTI3_MLreport.R
+++ utilities/SQANTI3_MLreport.R
@@ -1,3 +1,119 @@
+#!/bin/bash Rscript
+#
+#-------------------------------------------------------------------------------
+#                       _________________________
+#
+#                         SQANTI3 FILTER REPORT
+#                       _________________________
+#
+# Author: Ángeles Arzalluz-Luque
+# 
+# Contact: angeles.arzalluz@gmail.com
+#
+# Affiliation: Institute for Integrative Systems Biology, CSIC, Valencia, Spain
+#
+# Last updated: 13/July/2021
+#
+#-------------------------------------------------------------------------------
+
+
+#### Define script arguments ####
+
+option_list <- list(
+  optparse::make_option(c("-d","--dir"), type = "character", 
+                        help = "Output/input directory - must be same as SQ3 filter."),
+  optparse::make_option(c("-o","--output"), type = "character", default = "SQANTI3", 
+                        help = "Output report file prefix."),
+  optparse::make_option(c("f", "--filter_type"), type = "character",
+                        help = "Type of SQ3 filter that was run to generate input.")
+)
+
+# Parse arguments
+opt_parser = optparse::OptionParser(option_list = option_list)
+opt = optparse::parse_args(opt_parser)
+
+
+
+#### Initialize script and load input data ####
+
+message("\n-------------------------------------------------")
+message("\n \t SQANTI3 Machine Learning filter")
+message("\n--------------------------------------------------")
+
+# Import pipe operator
+require(magrittr)
+
+
+# Read files in output/input directory
+filter_outfiles <- dir(opt$dir)
+paths <- paste0(opt$dir, "/", filter_outfiles)
+
+
+# Detect files depending on filter type
+
+if(opt$filter_tpye == "ml"){
+  
+  # Detect path and load ML output classification
+  message("\nReading ML result classification table...")
+  
+  which_classif <- stringr::str_detect(paths, "MLresult_classification")
+  path_classif <- paths[which_classif]
+  classif <- readr::read_tsv(path_classif)
+  
+  
+  # Detect path and load variable importance table
+  message("\nReading classifier variable importance table...")
+  
+  which_imp <- stringr::str_detect(paths, "variable-importance_table")
+  path_imp <- paths[which_imp]
+  imp <- readr::read_tsv(path_imp, col_names = c("variable", "importance"))
+  
+  # format imp as factor
+  imp <- imp %>% dplyr::mutate(variable = factor(variable) %>% 
+                                 forcats::fct_reorder(importance))
+  
+}
+
+
+#### Define plot theme ####
+
+# Load ggplot2
+require(ggplot2)
+
+# Set theme parameters (from SQANTI3_report.R)
+sq_theme <- theme_classic(base_family = "Helvetica") +
+  theme(plot.title = element_text(lineheight=.4, size=15, hjust = 0.5)) +
+  theme(plot.margin = unit(c(2.5,1,1,1), "cm")) +
+  theme(axis.line.x = element_line(color="black", size = 0.4),
+        axis.line.y = element_line(color="black", size = 0.4)) +
+  theme(axis.title.x = element_text(size=13),
+        axis.text.x  = element_text(size=12),
+        axis.title.y = element_text(size=13),
+        axis.text.y  = element_text(vjust=0.5, size=12) ) +
+  theme(legend.text = element_text(size = 11), 
+        legend.title = element_text(size=12), 
+        legend.key.size = unit(0.5, "cm"))
+
+theme_set(sq_theme)
+
+
+
+#### START ML filter plots ####
+if(opt$filter_type == "ml"){
+  
+  
+    ## Variable importance in classifier
+    var_imp <- ggplot(imp) +
+      ggtitle("Variable importance in Random Forest classifier") +
+      geom_bar(aes(x = variable, y = importance), stat = "identity",
+               width = 0.7, color = "black", fill = "cadetblue3") +
+      labs(x = "SQANTI3 variables", y = "Importance") +
+      coord_flip()
+
+}
+#### END ML filter plots ####
+
+
 ##### Summary of results #####
 # plot.compare is a function to evaluate the values of the features used in the 
 # ML filter in Artifacts and Isoforms per structural category
