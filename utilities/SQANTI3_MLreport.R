8257f0b68dd9caff060fee2cecbe2d043b3facae
Angeles <angeles.arzalluz@gmail.com>
2021-07-20 17:38:22 +0200

    Create new function to compare ML variables, move to file and update report code

--- utilities/SQANTI3_MLreport.R
+++ utilities/SQANTI3_MLreport.R
@@ -135,7 +135,7 @@ category_summary <- classif %>%
         geom_bar(aes(alpha = filter_result, fill = structural_category), 
                   stat = "identity",
                   width = 0.8, color = "black", position = "dodge") +
-        labs(x = "Structural category", y = "Isoform no.") +
+        labs(x = "Structural category", y = "Transcript no.") +
         theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
         scale_fill_manual("", values = cat.palette, guide = "none") +
         scale_alpha_manual("Filter result", values = c(0.3, 1))
@@ -146,7 +146,7 @@ category_summary <- classif %>%
         geom_bar(aes(alpha = filter_result, fill = structural_category), 
                   stat = "identity",
                   width = 0.8, color = "black", position = "stack") +
-        labs(x = "Structural category", y = "Isoform %") +
+        labs(x = "Structural category", y = "Transcript %") +
         theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
         scale_y_continuous(labels = scales::percent_format()) +
         scale_fill_manual("", values = cat.palette, guide = "none") +
@@ -169,85 +169,16 @@ if(opt$filter_type == "ml"){
       coord_flip()
 
     
+    ## Variables used in ML: values for isoforms and artifacts by category
+    source(paste0(getwd(), "/utilities/compare_MLvariables.R"))
+    
+    var_compare <- purrr::map2(imp$variable, imp$importance,
+                               ~compare_MLvariables(classif, .x, .y))
+    
 }
 #### END ML filter plots ####
 
 
-##### Summary of results #####
-# plot.compare is a function to evaluate the values of the features used in the 
-# ML filter in Artifacts and Isoforms per structural category
-
-
-plot.compare <- function (classification, myfeature, imp) {
-  
-  require(ggplot2)
-  
-  mycategories = c("full-splice_match", "incomplete-splice_match", "novel_in_catalog",
-                   "novel_not_in_catalog", "intergenic", "fusion", "genic", 
-                   "antisense", "genic_intron")
-  mylabels = c("FSM", "ISM", "NIC", "NNC", "Inter", "Fus", "Gen", "Anti", "Intron")
-  col <- which(colnames(classification) == myfeature)
-  
-  
-  if (class(classification[,col]) != "factor") {
-    if (myfeature != "count")  {
-      dat <- data.frame(category = classification$structural_category, 
-                        filter = classification$MLfilter_result, 
-                        feature = classification[,col])
-      dat2 <- with(dat, tapply(feature,list(category,filter), median))
-    } else {
-      dat <- data.frame(category = classification$structural_category, 
-                        filter = classification$MLfilter_result)
-      dat2 <- table(dat)
-      dat2 <- dat2[mycategories,]
-    }
-    
-    dat3 <- reshape::melt(dat2) ; names(dat3) <- c("category", "filter", "feature")
-    dat3$category <- factor(dat3$category, levels = mycategories)
-    dat3$filter <- factor(dat3$filter, levels = c("Isoform", "Artifact"))
-    
-    p <- ggplot(dat3, aes(x = category, y =  feature)) +
-      geom_bar(
-        aes(color = filter, fill = filter),
-        stat = "identity", position = position_dodge(0.8)) +
-      scale_color_manual(values = c("#0073C2FF", "#EFC000FF")) +
-      scale_fill_manual(values = c("#0073C2FF", "#EFC000FF")) +
-      ggtitle(paste(myfeature, "importance", imp, sep = " ")) + 
-      scale_x_discrete(breaks = mycategories,
-                       labels = mylabels)
-    
-  } else {   # code for categorical features
-    col <- which(colnames(classification) == myfeature)
-    dat <- data.frame(category = classification$structural_category, 
-                      filter = classification$MLfilter_result, feature = classification[,col])
-    pasted <- as.data.frame(table(apply(dat,1, paste, collapse = "#")))
-    splitted <- strsplit(as.character(pasted$Var1), split = "#")
-    dat2 <- data.frame(matrix(unlist(splitted), nrow = length(splitted), byrow = TRUE))
-    names(dat2) <- c("category", "filter", "feature")
-    dat3 <- data.frame(dat2, value = pasted$Freq)
-    dat3$category <- factor(dat3$category, levels = mycategories)
-    dat3$filter <- factor(dat3$filter, levels = c("Isoform", "Artifact"))
-    dat3$labels <- mylabels [match(dat3$category,  mycategories)]
-    dat3$labels <- factor(dat3$labels, levels = mylabels)
-    
-    p <- ggplot(dat3, aes(x = filter, y = value, fill = feature)) + 
-      geom_bar(stat = 'identity', position = 'stack') + facet_grid(~ labels) +
-      ggtitle(paste(myfeature, "importance", imp, sep = " ")) 
-  }
-  
-  return(p)
-}
-
-features.eval <- c(rownames(imp), "intra_priming")
-importance <- c(round(imp[,1],2), "NA")
-
-pdf(file = paste0(opt$output_directory, "/SQANTI3_ML_report.pdf"))
-for (i in 1:length(features.eval)) {
-  message(paste(i, ":", features.eval[i]))
-  eval_p <- plot.compare(classification = d1, myfeature = features.eval[i], imp = importance[i])
-  print(eval_p)
-}
-dev.off()
 
 
 plot(density(log(d1$ratio_TSS[d1$structural_category == "incomplete-splice_match"])), col = "red")
---
+++ utilities/compare_MLvariables.R
@@ -0,0 +1,50 @@
+##### Summary of ML variables in artifacts and isoforms #####
+
+compare_MLvariables <- function(classification, variable, importance){
+  
+  # Select variable for evaluation plot
+  var_df <- classification %>% 
+    dplyr::select(structural_category, filter_result,
+                  dplyr::all_of(variable))
+  
+  # Get variable column info (class, name)
+  var_type <- purrr::map_chr(var_df, class)
+  var_name <- names(var_type[3])
+  
+  # Rename variable column to handle during plotting
+  var_df <- var_df %>% dplyr::rename(variable = variable)
+  
+  # Generate plot by type
+  if(var_type[3] == "numeric"){
+    
+    p <- ggplot(var_df) +
+      ggtitle(paste(var_name, "-", "ML importance:", importance)) +
+      geom_boxplot(aes(x = structural_category, y = log(abs(variable)+1),
+                       fill = filter_result)) +
+      labs(x = "Structural category", y = paste0("log( |", var_name, "| +1)")) +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+      scale_fill_manual("Filter result", 
+                        values = c("deepskyblue3", "darkgoldenrod2"))
+    
+    return(p)
+    
+  } else{
+    
+    var_df <- var_df %>% 
+      dplyr::filter(!is.na(variable)) %>% 
+      dplyr::mutate(variable = factor(variable))
+    
+    p <- ggplot(var_df) +
+      ggtitle(paste(var_name, "-", "ML importance:", importance)) +
+      geom_bar(aes(x = filter_result, fill = variable), stat = "count", 
+               width = 0.8, color = "black", position = "dodge") +
+      labs(x = "Filter result", y = "Transcript no.") +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+      RColorConesa::scale_fill_conesa(paste0(var_name), reverse = FALSE,
+                                      palette = "complete", discrete = TRUE) +
+      facet_grid(~structural_category, scales = "free")
+    
+    return(p)
+    
+  }
+}
