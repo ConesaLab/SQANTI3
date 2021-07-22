e09807114eaf331bb6edade571221650f594d956
Angeles <angeles.arzalluz@gmail.com>
2021-07-22 17:01:47 +0200

    Minor adjustments to plots in compare_MLvariables() function

--- utilities/compare_MLvariables.R
+++ utilities/compare_MLvariables.R
@@ -2,6 +2,9 @@
 
 compare_MLvariables <- function(classification, variable, importance){
   
+  require(ggplot2)
+  require(magrittr)
+  
   # Select variable for evaluation plot
   var_df <- classification %>% 
     dplyr::select(structural_category, filter_result,
@@ -14,13 +17,16 @@ compare_MLvariables <- function(classification, variable, importance){
   # Rename variable column to handle during plotting
   var_df <- var_df %>% dplyr::rename(variable = variable)
   
+  # Round importance
+  importance <- round(importance, 2)
+  
   # Generate plot by type
   if(var_type[3] == "numeric"){
     
     p <- ggplot(var_df) +
       ggtitle(paste(var_name, "-", "ML importance:", importance)) +
       geom_boxplot(aes(x = structural_category, y = log(abs(variable)+1),
-                       fill = filter_result)) +
+                       fill = filter_result), outlier.size = 0.2) +
       labs(x = "Structural category", y = paste0("log( |", var_name, "| +1)")) +
       theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
       scale_fill_manual("Filter result", 
