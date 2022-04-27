5f37512fc0ccb83cb9133e29c354686946ccabd9
Angeles <angeles.arzalluz@gmail.com>
2022-04-27 11:55:40 +0200

    Update scripts to run SQANTI3 QC and ML filter on example data

--- example/run_MLfilter-Rscript.sh
+++
@@ -1,6 +0,0 @@
-# Command to generate ML filter outputs from SQANTI3 output
-# Only includes mandatory arguments, all parameters set to default
-# Run from SQANTI3 directory!
-
-Rscript utilities/SQANTI3_MLfilter.R -c example/SQANTI3_output/UHR_chr22_classification.txt -o UHR_chr22 -d example/MLfilter_output 
-
