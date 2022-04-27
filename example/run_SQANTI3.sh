7f7688b1b735bcfc650a7b4044cdad58382af4f6
Angeles <angeles.arzalluz@gmail.com>
2022-04-27 11:55:40 +0200

    Update scripts to run SQANTI3 QC and ML filter on example data

--- example/run_SQANTI3.sh
+++
@@ -1,3 +0,0 @@
-## This is the command used to create the SQANTI3 output for the example data
-
-sqanti3_qc.py example/UHR_chr22.gtf example/gencode.v38.basic_chr22.gtf example/GRCh38.p13_chr22.fasta --cage_peak data/ref_TSS_annotation/human.refTSS_v3.1.hg38.bed --polyA_motif_list data/polyA_motifs/mouse_and_human.polyA_motif.txt -o UHR_chr22 -d example/SQANTI3_output -fl example/UHR_abundance.tsv --short_reads example/UHR_chr22_short_reads.fofn --cpus 4 --report both
