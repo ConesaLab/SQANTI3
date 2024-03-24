f9b74fff6619723cd7ef0c0e0aed3cd8e45beb9a
Mark Diekhans <markd@ucsc.edu>
2024-03-24 10:59:01 -0700

    Revert "sys.path updates for programs"
    This reverts commit 68330bff9ec2e999c3c844cb931d99c3b2084e61.

--- sqanti3_filter.py
+++ sqanti3_filter.py
@@ -26,11 +26,8 @@ import distutils.spawn
 from csv import DictReader, DictWriter
 from Bio import SeqIO
 
-rootdir = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, os.path.join(rootdir, "utilities"))
-sys.path.insert(0, os.path.join(rootdir, "cupcake"))
-from cupcake.io.BioReaders import GMAPSAMReader
-from cupcake.io.GFF import collapseGFFReader, write_collapseGFF_format
+from utilities.cupcake.io.BioReaders import GMAPSAMReader
+from utilities.cupcake.io.GFF import collapseGFFReader, write_collapseGFF_format
 
 utilitiesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "utilities")
 RSCRIPTPATH = distutils.spawn.find_executable('Rscript')
