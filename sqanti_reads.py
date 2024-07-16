f75b09b293734bfefbc8063f24264287cd832ee3
Carolina  Monzó <carolmonzoc@gmail.com>
2024-07-16 18:46:13 +0200

    adding multisample functionality

--- sqanti_reads.py
+++ sqanti_reads.py
@@ -1,5 +1,5 @@
 #!usr/bin/python
-import subprocess, os, re, sys
+import subprocess, os, re, sys, glob
 import argparse
 import pandas as pd
 import distutils.spawn
@@ -9,7 +9,7 @@ import hashlib
 # Author: Carolina Monzo
 
 __author__  = "carolina.monzo@csic.es"
-__version__ = '5.2'  # Python 3.7
+__version__ = '1.0'  # Python 3.7
 
 
 utilitiesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "utilities")
@@ -19,97 +19,143 @@ utilitiesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "utili
 sqantiqcPath = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
 
-FIELDS_JUNC = ['read', 'chrom', 'strand', 'junction_number', 'genomic_start_coord',
+FIELDS_JUNC = ['isoform', 'chrom', 'strand', 'junction_number', 'genomic_start_coord',
                    'genomic_end_coord', 'junction_category',
                    'diff_to_Ref_start_site', 'diff_to_Ref_end_site', 'canonical']
 
-FIELDS_CLASS = ['read', 'chrom', 'strand', 'length',  'exons',  'structural_category',
+FIELDS_CLASS = ['isoform', 'chrom', 'strand', 'length',  'exons',  'structural_category',
                 'associated_gene', 'associated_transcript',  'ref_length', 'ref_exons',
                 'subcategory', 'RTS_stage', 'all_canonical',
-                'predicted_NMD', 'perc_A_downstream_TTS', "Jxn_string"]
+                'predicted_NMD', 'perc_A_downstream_TTS', "jxn_string"]
 
 RSCRIPTPATH = distutils.spawn.find_executable('Rscript')
 
-def run_sqantiReads(args):
-    outputPathPrefix = os.path.join(args.dir, args.output)
-    
-    if args.classification is not None:
-        # Reduce the junctions file
-        cmd_reduceJunctions = f"cat {outputPathPrefix}_junctions.txt | cut -f 1,2,3,4,5,6,8,11,12,15 > {outputPathPrefix}_r_junctions.txt"
-
-        if subprocess.check_call(cmd_reduceJunctions, shell = True) !=0:
-            print("Problem reducing the junction file")
-            sys.exit(-1)
-        os.remove(f"{outputPathPrefix}_junctions.txt")
-        cmd_sed = f"sed '1s/isoform/read/' {outputPathPrefix}_r_junctions.txt > {outputPathPrefix}_reads_junctions.txt"
-        subprocess.call(cmd_sed, shell = True)
-        os.remove(f"{outputPathPrefix}_r_junctions.txt")
-
-
-    print("**** Calculating UJCs...", file = sys.stdout)
-            
-    ## Take the corrected GTF
-    introns_cmd = f"gtftools -i {outputPathPrefix}tmp_introns.bed {outputPathPrefix}_corrected.gtf"
-    ujc_cmd = f"""awk -F'\t' -v OFS="\t" '{{print $5,"chr"$1,$4,$2+1"_"$3}}' {outputPathPrefix}tmp_introns.bed | bedtools groupby -g 1 -c 2,3,4 -o distinct,distinct,collapse | sed 's/,/_/g' | awk -F'\t' -v OFS="\t" '{{print $1,$2"_"$3"_"$4}}' > {outputPathPrefix}tmp_UJC.txt"""
-        
-    if subprocess.check_call(introns_cmd, shell=True)!=0:
-        print("ERROR running command: {0}\n Missing GTFTOOLS".format(introns_cmd), file=sys.stderr)
+def fill_design_table(args):
+    df = pd.read_csv(args.inDESIGN, sep = ",")
+    # If number of columns is less than 2, probably wrongly formatted
+    if df.shape[1] < 2:
+        print("ERROR: is incorrectly formatted, is it not separated by commas?".format(args.inDESIGN), file=sys.stderr)
         sys.exit(-1)
-        
-    if os.path.exists(f"{outputPathPrefix}_corrected.gtf.ensembl"):
-        os.remove(f"{outputPathPrefix}_corrected.gtf.ensembl")
-        
-    if subprocess.check_call(ujc_cmd, shell=True)!=0:
-        print("ERROR running command: {0}\n Missing BEDTOOLS".format(introns_cmd), file=sys.stderr)
-        sys.exit(-1)
-    os.remove(f"{outputPathPrefix}tmp_introns.bed")
-
-    # Reduce the classification file
-    if args.classification is not None:
-        classfile = f"{outputPathPrefix}_r_classification.txt"
-        cmd_reduceClassification = f"cat {outputPathPrefix}_classification.txt | cut -f 1,2,3,4,5,6,7,8,9,10,15,16,17,37,38 | sed '1s/isoform/read/' > {outputPathPrefix}_r_classification.txt"
-        if subprocess.check_call(cmd_reduceClassification, shell = True) !=0:
-            print("Problem reducing the classification file")
-            sys.exit(-1)
-    else:
-        classfile = f"{outputPathPrefix}_classification.txt"
-        
-
-    ## Pandas merge to the left
-    clas_df = pd.read_csv(classfile, sep = "\t", usecols = [0, 1, 2, 7])
-    clas_df.columns = ["read", "chr", "strand", "associated_transcript"]
-    ujc_df = pd.read_csv(f"{outputPathPrefix}tmp_UJC.txt", sep = "\t", names = ["read", "jxn_string"])
     
-    merged_df = pd.merge(clas_df, ujc_df, on = "read", how = "left")
-    
-    # Fill missing values in UJC column using the transcript ID
-    merged_df["jxn_string"] = merged_df.apply(lambda row: row["chr"] + "_" + row["strand"] + "_" + "monoexon" + "_" + row["associated_transcript"] if pd.isna(row["jxn_string"]) else row["jxn_string"], axis=1)
-    
-    merged_df['jxn_hash'] = merged_df['jxn_string'].apply(
-                lambda x: hashlib.sha256(x.encode('utf-8')).hexdigest())
+    # Create the new columns
+    df['classification_file'] = args.input_dir + '/' + df['file_acc'] + '/' + df['sampleID'] + '_reads_classification.txt'
+    df['junction_file'] = args.input_dir + '/' + df['file_acc'] + '/' + df['sampleID'] + '_junctions.txt'
+    df.to_csv(args.inDESIGN, sep = ',', index = False)
+    return(df)
 
-    #if not merged_df['jxn_hash'].is_unique:
-    #    print ("Wow! A rare jxnHash collision: two jxnStrings have resulted in the exact same hash for these reads and transcripts: ")
+def get_method_runSQANTI3(args, df):
+    
+    for index, row in df.iterrows():
+        file_acc = row['file_acc']
+        sampleID = row['sampleID']
+        classification_file = os.path.join(args.input_dir, file_acc, f"{sampleID}_classification.txt")
+        junction_file = row["junction_file"]
+
+        # Check for directory containing classification and junction file
+        directory_path = os.path.join(args.input_dir, file_acc)
+        if os.path.isdir(directory_path):
+            classification_file_path = os.path.join(classification_file)
+            junction_file_path = os.path.join(junction_file)
+            if os.path.isfile(classification_file_path) and os.path.isfile(junction_file_path):
+                if args.verbose:
+                    print(f"[INFO] You inputted SQANTI3 directories, we will run sqanti_reads in fast mode for sample {directory_path}", file=sys.stdout)
+                continue
+        
+        # Check for .gtf file
+        gtf_pattern = os.path.join(args.input_dir, f"{file_acc}*.gtf")
+        try:
+            gtf_files = glob.glob(gtf_pattern)[0]
+        except IndexError:
+            pass
+        else:
+            if os.path.isfile(gtf_files):
+                if os.path.isfile(args.genome) is False:
+                    print(f'[ERROR] You inputted gtf files to run SQANTI3 but no reference genome FASTA', file=sys.stdout)
+                    sys.exit(-1)
+                if os.path.isfile(args.annotation) is False:
+                    print(f'[ERROR] You inputted gtf files to run SQANTI3 but no reference annotation GTF', file=sys.stdout)
+                    sys.exit(-1)
+                if args.verbose:
+                    print(f'[INFO] You inputted gtfs, we will run sqanti_reads in simple mode for sample {gtf_files}', file=sys.stdout)
+                cmd_sqanti = f"python {sqantiqcPath}/sqanti3_qc.py {gtf_files} {args.annotation} {args.genome} --skipORF --min_ref_len {args.min_ref_len} --aligner_choice {args.aligner_choice} -t {args.cpus} -d {args.input_dir}/{file_acc} -o {sampleID} -s {args.sites}"
+                if args.force_id_ignore:
+                    cmd_sqanti = cmd_sqanti + " --force_id_ignore True"
+                subprocess.call(cmd_sqanti, shell = True)
+                continue
+
+        # Check for .fastq files
+        fastq_pattern = os.path.join(args.input_dir, f"{file_acc}*.fastq")
+        try:
+            fastq_files = glob.glob(fastq_pattern)[0]
+        except IndexError:
+            pass
+        else:
+            if os.path.isfile(fastq_files):
+                if os.path.isfile(args.genome) is False:
+                    print(f'[ERROR] You inputted fastq files to map but no reference genome FASTA', file=sys.stdout)
+                    sys.exit(-1)
+                if os.path.isfile(args.annotation) is False:
+                    print(f'[ERROR] You inputted fastq files to map but no reference annotation GTF', file=sys.stdout)
+                    sys.exit(-1)
+                if args.verbose:
+                    print(f'[INFO] You inputted reads, we will run sqanti_reads in simple mode for sample {fastq_files}', file=sys.stdout)
+                cmd_sqanti = f"python {sqantiqcPath}/sqanti3_qc.py {fastq_files} {args.annotation} {args.genome} --skipORF --min_ref_len {args.min_ref_len} --aligner_choice {args.aligner_choice} -t {args.cpus} -d {args.input_dir}/{file_acc} -o {sampleID} -s {args.sites} --fasta"
+                if args.force_id_ignore:
+                    cmd_sqanti = cmd_sqanti + " --force_id_ignore True"
+                subprocess.call(cmd_sqanti, shell = True)
+                continue
+        
+        # If none of the conditions are met, raise an error
+        print(f"ERROR: The file_acc you included in your design file does not correspond to .fastq, .gtf or directories with junctions and classification files in the {args.input_dir} directory", file=sys.stdout)
+
+
+def make_UJC_hash(args, df):
+
+    for index, row in df.iterrows():
+        file_acc = row['file_acc']
+        sampleID = row['sampleID']
+        # Input dir, sqanti3 dir, samplename
+        outputPathPrefix = os.path.join(args.input_dir, file_acc, sampleID)
+
+        print("**** Calculating UJCs...", file = sys.stdout)
                 
-    #    duplicateDf = merged_df[merged_df.duplicated(subset='jxnHash',keep=False) | merged_df.duplicated(subset='jxnHash',keep='first')]
-    #    for row in duplicateDf.to_dict('records'):
-    #        print(row['read'],row['associated_transcript'])
-    
-    merged_df.to_csv(f"{outputPathPrefix}_temp.txt", index = False, sep = "\t")
-    
-    cmd_paste = ['bash -c "paste <(cat ', classfile, ') <(cut -f 5,6 ', outputPathPrefix, '_temp.txt) > ', outputPathPrefix, '_reads_classification.txt"']
-    subprocess.call("".join(cmd_paste), shell = True)
-    
-    if args.classification is not None:
-        os.remove(f"{outputPathPrefix}_r_classification.txt")
-    
-    os.remove(f"{outputPathPrefix}tmp_UJC.txt")
-    #os.remove(f"{outputPathPrefix}_temp.txt")
-    os.remove(f"{outputPathPrefix}_classification.txt")
+        ## Take the corrected GTF
+        introns_cmd = f"gtftools -i {outputPathPrefix}tmp_introns.bed {outputPathPrefix}_corrected.gtf"
+        ujc_cmd = f"""awk -F'\t' -v OFS="\t" '{{print $5,"chr"$1,$4,$2+1"_"$3}}' {outputPathPrefix}tmp_introns.bed | bedtools groupby -g 1 -c 2,3,4 -o distinct,distinct,collapse | sed 's/,/_/g' | awk -F'\t' -v OFS="\t" '{{print $1,$2"_"$3"_"$4}}' > {outputPathPrefix}tmp_UJC.txt"""
+            
+        if subprocess.check_call(introns_cmd, shell=True)!=0:
+            print("ERROR running command: {0}\n Missing GTFTOOLS".format(introns_cmd), file=sys.stderr)
+            sys.exit(-1)
+            
+        if os.path.exists(f"{outputPathPrefix}_corrected.gtf.ensembl"):
+            os.remove(f"{outputPathPrefix}_corrected.gtf.ensembl")
+            
+        if subprocess.check_call(ujc_cmd, shell=True)!=0:
+            print("ERROR running command: {0}\n Missing BEDTOOLS".format(introns_cmd), file=sys.stderr)
+            sys.exit(-1)
+        os.remove(f"{outputPathPrefix}tmp_introns.bed")
 
-
-
-
+        ## Pandas merge to the left
+        classfile = f"{outputPathPrefix}_classification.txt"
+        clas_df = pd.read_csv(classfile, sep = "\t", usecols = [0, 1, 2, 7])
+        clas_df.columns = ["isoform", "chr", "strand", "associated_transcript"]
+        ujc_df = pd.read_csv(f"{outputPathPrefix}tmp_UJC.txt", sep = "\t", names = ["isoform", "jxn_string"])
+        
+        merged_df = pd.merge(clas_df, ujc_df, on = "isoform", how = "left")
+        
+        # Fill missing values in UJC column using the transcript ID
+        merged_df["jxn_string"] = merged_df.apply(lambda row: row["chr"] + "_" + row["strand"] + "_" + "monoexon" + "_" + row["associated_transcript"] if pd.isna(row["jxn_string"]) else row["jxn_string"], axis=1)
+        
+        merged_df['jxnHash'] = merged_df['jxn_string'].apply(
+                    lambda x: hashlib.sha256(x.encode('utf-8')).hexdigest())
+        
+        merged_df.to_csv(f"{outputPathPrefix}_temp.txt", index = False, sep = "\t")
+        
+        cmd_paste = f"""bash -c 'paste <(cat {classfile} | tr -d '\r') <(cut -f 5,6 {outputPathPrefix}_temp.txt | tr -d '\r') > {outputPathPrefix}_reads_classification.txt'"""
+        subprocess.call(cmd_paste, shell = True)
+        
+        os.remove(f"{outputPathPrefix}tmp_UJC.txt")
+        os.remove(f"{outputPathPrefix}_temp.txt")
 
 def main():
     global utilitiesPath
@@ -117,40 +163,54 @@ def main():
 
     #arguments
     parser = argparse.ArgumentParser(description="Structural and Quality Annotation of Novel Transcript Isoforms")
-    parser.add_argument('--reads', default = None, help='\Reads (FASTA/FASTQ) or GTF format. It is recommended to provide them in GTF format, but if it is needed to map the sequences to the genome use a FASTA/FASTQ file with the --fasta option.')
-    parser.add_argument('--annotation', help='\t\tReference annotation file (GTF format)', default = None)
-    parser.add_argument('--genome', help='\t\tReference genome (Fasta format)', default = None)
+    parser.add_argument('-de', '--design', type=str, dest="inDESIGN" ,required=True, help='Path to design file, must have sampleID column, classification_file column and junction_file column')
+    parser.add_argument('-f', '--factor', type=str, dest="inFACTOR" ,required=False, help='Optional: This is the column name that plots are to be faceted by')
+    parser.add_argument('--annotation', type=str, help='\t\tReference annotation file (GTF format)', default = False, required = True)
+    parser.add_argument('--genome', type=str, help='\t\tReference genome (Fasta format)', default = False, required = False)
+    parser.add_argument('-p','--prefix', type=str, dest="PREFIX", required=False, help='SQANTI-reads output filename prefix')
+    parser.add_argument('-d','--dir', type=str, help='\t\tDirectory for output sqanti_reads files. Default: Directory where the script was run.', default = "./", required=False)
     parser.add_argument('--min_ref_len', type=int, default=0, help="\t\tMinimum reference transcript length (default: 0 bp)")
     parser.add_argument('--force_id_ignore', action="store_true", default=False, help="\t\t Allow the usage of transcript IDs non related with PacBio's nomenclature (PB.X.Y)")
-    parser.add_argument('--aligner_choice', choices=['minimap2', "uLTRA"], default='minimap2')
-    parser.add_argument('-o','--output', help='\t\tPrefix for output files.', required=False)
+    parser.add_argument('--aligner_choice', type=str, choices=['minimap2', "uLTRA"], default='minimap2')
     parser.add_argument('-t', '--cpus', default=10, type=int, help='\t\tNumber of threads used during alignment by aligners. (default: 10)')
     parser.add_argument('-n', '--chunks', default=1, type=int, help='\t\tNumber of chunks to split SQANTI3 analysis in for speed up (default: 1).')
-    parser.add_argument('-d','--dir', help='\t\tDirectory for output files. Default: Directory where the script was run.', required=False)
-    parser.add_argument('-s','--sites', default="ATAC,GCAG,GTAG", help='\t\tSet of splice sites to be considered as canonical (comma-separated list of splice sites). Default: GTAG,GCAG,ATAC.', required=False)
-    parser.add_argument('-v', '--version', help="Display program version number.", action='version', version='SQANTI3 '+str(__version__))
-    parser.add_argument('--saturation', action="store_true", default=False, help='\t\tInclude saturation curves into report')
-    parser.add_argument('--fasta', help='\t\tUse when running SQANTI by using as input a FASTA/FASTQ with the sequences of isoforms', action='store_true')
+    parser.add_argument('-s','--sites', type=str, default="ATAC,GCAG,GTAG", help='\t\tSet of splice sites to be considered as canonical (comma-separated list of splice sites). Default: GTAG,GCAG,ATAC.', required=False)
+    parser.add_argument('-gp', '--generate_plots', action='store_true', dest="GENERATE_PLOTS", help='Generate plots if set (default: True)', default=True)
+    parser.add_argument('-ge','--gene_expression', type=int, dest="ANNOTEXP", required=False, help='Expression cut off level for determining underannotated genes', default = 10)
+    parser.add_argument('-je','--jxn_expression', type=int, dest="JXNEXP", required=False, help='Expression cut off level for junction cv plots', default = 3)
+    parser.add_argument('-pn','--perc_novel', type=int, dest="PERCNOVEL", required=False, help='Percent NIC+NNC for determining underannotated genes', default = 90)
+    parser.add_argument('-i', '--input_dir', type=str, default = './', help = '\t\tPath to directory where fastq/GTF files are stored. Or path to parent directory with children directories of SQANTI3 runs.')
+    parser.add_argument('--verbose', help = 'If verbose is run, it will print all steps, by default it is FALSE', action="store_true")
+    parser.add_argument('-v', '--version', help="Display program version number.", action='version', version='sqanti-reads '+str(__version__))
+
     args = parser.parse_args()
 
-    ## If they give us a classification file, don't run sqanti, run only sqanti_reads
-    if args.reads is not None and args.annotation is not None and args.genome is not None:
-        cmd_sqanti = f"python3 {sqantiqcPath}sqanti_qc.py {args.reads} {args.annotation} {args.genome} --min_ref_len {args.min_ref_len} --force_id_ignore {args.force_id_ignore} --aligner_choice {args.aligner_choice} -t {args.cpus} -d {args.dir} -o {args.output} -s {args.sites} -v {args.version} --saturation {args.saturation} --fasta {args.fasta}"
-            
-        subprocess.call(cmd_sqanti, shell = True)
-        args.classification = "caca"
+    # Check and read design file
+    df = fill_design_table(args)
+
+    # Check method and run SQANTI3
+    get_method_runSQANTI3(args, df)
+
+    # Make UJC and hash
+    make_UJC_hash(args, df)
+
+
+
+    # Run plotting script
+    plotting_script_path = os.path.join(os.path.dirname(__file__), 'utilities', 'sqanti_reads_tables_and_plots_02ndk.py')
+
+    cmd_plotting = f"python {plotting_script_path} --ref {args.annotation} --design {args.inDESIGN} -o {args.dir} --gene-expression {args.ANNOTEXP} --jxn-expression {args.JXNEXP} --perc-novel {args.PERCNOVEL}"
+    if args.inFACTOR:
+        cmd_plotting = cmd_plotting + f" --factor {args.inFACTOR}"
+    if args.PREFIX:
+        cmd_plotting = cmd_plotting + f" --prefix {args.PREFIX}"
     else:
-        args.classification = None
+        cmd_plotting = cmd_plotting + " --prefix sqantiReads"
+    if args.GENERATE_PLOTS:
+        cmd_plotting = cmd_plotting + " --generate-plots"
     
-    ## Check that there is a classification and a corrected.gtf file in the output directory
-    if os.path.exists(os.path.join(args.dir, f"{args.output}_classification.txt")) and os.path.exists(os.path.join(args.dir, f"{args.output}_corrected.gtf")):
-        ### Run SQANTI READS ###
-        run_sqantiReads(args)
-        print("SQANTI-reads run complete")
-    else:
-        print(f"ERROR: There are no classification.txt and corrected.gtf files in your output directory: {args.dir}")
-    
-        
+    subprocess.call(cmd_plotting, shell = True)
+
 
 if __name__ == "__main__":
     main()
