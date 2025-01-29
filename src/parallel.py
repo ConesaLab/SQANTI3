a285abf95f869cfd249afd1edadaacb01d104b60
pabloati <pabloatienzalo@gmail.com>
2025-01-29 15:01:58 +0100

    Fixed parallelization bug 388

--- src/parallel.py
+++ src/parallel.py
@@ -9,8 +9,8 @@ from src.qc_computations import classify_fsm, full_length_quantification, proces
 from .utilities.cupcake.io.GFF import collapseGFFReader, write_collapseGFF_format
 
 from .qc_pipeline import run
-from .helpers import get_corr_filenames, get_class_junc_filenames, get_pickle_filename, rename_novel_genes
-from .qc_output import generate_report, write_classification_output, write_isoform_hits, write_junction_output, write_omitted_isoforms
+from .helpers import get_corr_filenames, get_class_junc_filenames, get_pickle_filename, rename_novel_genes, write_collapsed_GFF_with_CDS
+from .qc_output import cleanup, generate_report, write_classification_output, write_isoform_hits, write_junction_output, write_omitted_isoforms
 # TODO: Do the split based on isoform ID groups, not on pure numbers
 
 
@@ -153,12 +153,11 @@ def combine_split_runs(args, split_dirs):
     f_fasta = open(corrFASTA, 'w')
     f_gtf = open(corrGTF, 'w')
     f_junc_temp = open(outputJuncPath+"_tmp", "w")
-    f_cds_gtf_gff = open(corrCDS_GTF_GFF, 'w')
     isoforms_info = {}
     headers = []
     
     for i,split_d in enumerate(split_dirs):
-        _gtf, _, _fasta, _orf , _CDS_GTF_GFF = get_corr_filenames(split_d,args.output)
+        _gtf, _, _fasta, _orf , _ = get_corr_filenames(split_d,args.output)
         _, _junc = get_class_junc_filenames(split_d,args.output)
         _info = get_pickle_filename(split_d,args.output)
         if not args.skipORF:
@@ -175,17 +174,10 @@ def combine_split_runs(args, split_dirs):
         with open(_info, 'rb') as h:
             isoforms_info.update(pickle.load(h))
             headers.append(pickle.load(h))
-        with open(_CDS_GTF_GFF) as h:
-            if i == 0: # This if condition checks if its the first file to write the header or not in the final file
-                f_cds_gtf_gff.write(h.readline())
-            else:
-                h.readline()
-            f_cds_gtf_gff.write(h.read())
 
     f_fasta.close()
     f_gtf.close()
     f_junc_temp.close()
-    f_cds_gtf_gff.close()
     # Fix novel genes and classify FSM
     isoforms_info = rename_novel_genes(isoforms_info, args.novel_gene_prefix)
     isoforms_info = classify_fsm(isoforms_info)
@@ -196,13 +188,13 @@ def combine_split_runs(args, split_dirs):
     isoforms_info,RTS_info = process_rts_swiching(isoforms_info,outputJuncPath,args.genome)
 
     fields_junc_cur = headers[0]
+    write_collapsed_GFF_with_CDS(isoforms_info, corrGTF, corrCDS_GTF_GFF)
     write_classification_output(isoforms_info, outputClassPath, FIELDS_CLASS)
-    print(fields_junc_cur)
     write_junction_output(outputJuncPath, RTS_info, fields_junc_cur)
     #write omitted isoforms if requested minimum reference length is more than 0
     isoforms_info = write_omitted_isoforms(isoforms_info, args.dir, args.output, 
                                             args.min_ref_len, args.is_fusion, fields_class_cur)
-        
+    cleanup(outputClassPath, outputJuncPath)
     #isoform hits to file if requested
     if args.isoform_hits:
         write_isoform_hits(args.dir, args.output, isoforms_info)
