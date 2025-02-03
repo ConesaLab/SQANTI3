8fdf52f80158b732f800d407d8ec633e8ea36913
Sean Bresnahan <seantbresnahan3@gmail.com>
2025-02-03 12:52:28 -0600

    Update sqanti3_filter.py
    sqanti3_filter.py line 32 should direct to src/utilities to be compatible with GitHub repository directory structure.

--- sqanti3_filter.py
+++ sqanti3_filter.py
@@ -29,7 +29,7 @@ from Bio import SeqIO
 from src.utilities.cupcake.io.BioReaders import GMAPSAMReader
 from src.utilities.cupcake.io.GFF import collapseGFFReader, write_collapseGFF_format
 
-utilitiesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "utilities")
+utilitiesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "utilities/src")
 RSCRIPTPATH = shutil.which('Rscript')
 RSCRIPT_REPORT = 'report_filter/SQANTI3_filter_report.R'
 RSCRIPT_ML = 'filter/SQANTI3_MLfilter.R'
