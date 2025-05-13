8bdd9d09c625b2b22864a039cc04f13f717071f0
pabloati <pabloatienzalo@gmail.com>
2025-05-13 16:39:05 +0200

    Minor changes

--- sqanti3_rescue.py
+++ sqanti3_rescue.py
@@ -17,11 +17,11 @@ from src.logging_config import rescue_art, art_logger
 
 from src.argparse_utils import rescue_args_validation
 from src.config import __version__
-from src.rescue_output import write_rescue_fasta, write_rescue_gtf
 from src.rescue_steps import (
   run_automatic_rescue,
   rescue_candidates, rescue_targets,
-  run_candidate_mapping, run_rules_rescue, run_ML_rescue
+  run_candidate_mapping, run_rules_rescue, run_ML_rescue,
+  save_rescue_results
 )
 from src.utilities.rescue import sq_requant
 from src.utilities.rescue.candidate_mapping_helpers import prepare_fasta_transcriptome
@@ -108,35 +108,19 @@ def main():
     rescue_logger.warning("Rescue will be performed but no GTF will be generated.")
   else:
     message("Generating rescued GTF.",rescue_logger)
+    rescued_list,rescue_gtf_path = save_rescue_results(args.dir, args.output, args.mode,
+                                       args.refGTF, args.filtered_isoforms_gtf)
 
-    # Select the propper inclusion list
-    if args.mode == "full":
-        rescued_list = f"{prefix}_full_inclusion_list.tsv"
-    else:
-        rescued_list = f"{prefix}_automatic_inclusion_list.tsv"
-    # Read the rescued transcripts from the inclusion list
-    rescued_transcripts = set()
-    with open(rescued_list, 'r') as f:
-      for line in f:
-        rescued_transcripts.add(line.strip())
-    f.close()    
-    write_rescue_gtf(args.rescue_gtf, args.refGTF, rescued_transcripts, prefix)
-    rescue_logger.info(f"Final output GTF written to file:  {prefix}_rescued.gtf")
-    
-    ## Create new FASTA including rescued transcripts #
-    good_transcripts = get_good_transcripts(args.filter_class)
-    ref_trans_fasta = os.path.join(args.dir, 
-                                  os.path.basename(args.refGTF).replace('.gtf', '.fasta'))
-    write_rescue_fasta(args.rescue_isoforms,ref_trans_fasta, good_transcripts, rescued_transcripts, prefix)
-    rescue_logger.info(f"Rescued FASTA written to file: {prefix}_rescued.fasta")
   ## END ##
   message("Rescue finished successfully!",rescue_logger)
 
   if args.requant:  
     message("Running requantification.",rescue_logger)
+    
+    rescue_gtf, counts, rescued_table = sq_requant.parse_files(rescue_gtf_path,args.counts, prefix)
 
-    rescue_gtf, inclusion_list, counts, rescued = sq_requant.parse_files(args)
-    sq_requant.run_requant(rescue_gtf, inclusion_list, counts, rescued, args.dir, args.output)
+    sq_requant.run_requant(rescue_gtf, rescued_list, counts, 
+                           rescued_table, prefix)
     sq_requant.to_tpm(rescue_gtf, args.dir, args.output)
     rescue_logger.info("Requantification finished!")
 
