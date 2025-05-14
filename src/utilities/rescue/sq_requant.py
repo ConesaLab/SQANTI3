7a4c9f16893ec1f9ce6645c11721e271fa81fdab
pabloati <pabloatienzalo@gmail.com>
2025-05-14 16:04:09 +0200

    Requantificaiton funcitonal. Not tested

--- src/utilities/rescue/sq_requant.py
+++ src/utilities/rescue/sq_requant.py
@@ -5,7 +5,7 @@ import sys
 import warnings
 warnings.filterwarnings("ignore")
 
-def parse_files(gtf_path,count_file,prefix):
+def parse_files(gtf_path,inclusion_file,count_file,prefix):
 
     col_names = [
         "Chromosome", "Source", "Feature", "Start", "End", "Score", "Strand", "Frame", "Attribute"
@@ -20,6 +20,9 @@ def parse_files(gtf_path,count_file,prefix):
     )
     rescue_gtf["transcript_id"] = rescue_gtf["Attribute"].str.extract(r'transcript_id "([^"]+)"')
     
+    # Inclusion list
+    inclusion_df = pd.read_csv(inclusion_file, names = ['transcript_id'])
+
     # Load counts file
     counts = pd.read_csv(count_file, sep = '\t', comment = '#')
     counts.columns = ['transcript_id', 'count']
@@ -28,7 +31,8 @@ def parse_files(gtf_path,count_file,prefix):
     rescued_path=f"{prefix}_rescue_table.tsv"
     rescued_table = pd.read_csv(rescued_path, sep = '\t')
 
-    return(rescue_gtf, counts, rescued_table)
+
+    return(rescue_gtf, inclusion_df, counts, rescued_table)
 
 def select_hit(isoform, rescued, new_counts, old_counts):
     group = rescued[rescued["mapping_hit"] == isoform]
@@ -54,16 +58,36 @@ def fill_old_counts(isoform, old_counts):
         count = 0
     return(count)
     
-def run_requant(rescue_gtf, inclusion_list, counts, 
+def run_requant(rescue_gtf, inclusion_df, counts, 
                 rescue_df, prefix):
 
-    inclusion_list = pd.DataFrame(inclusion_list, columns=["transcript_id"])
 
     #select list of isoforms that were rescued by SQANTI3 rescue
     rescued_iso = rescue_df[rescue_df['rescue_result'] != 'not_rescued']
     additional_isoforms = rescue_df[rescue_df['exclusion_reason'] == 'reference_already_present']
     rescued = pd.concat([rescued_iso, additional_isoforms])
-    rescued = rescued.drop_duplicates(['rescue_candidate', 'mapping_hit'])      
+
+    # Selec only isoforms that were not rescued or had a transcript that was already rescued
+    summary_df = (
+    rescue_df.groupby('rescue_candidate', as_index=False
+                     ).apply(lambda g: pd.Series({
+        'rescued': ((g['rescue_result'] != 'not_rescued') | 
+                    (g['exclusion_reason'].fillna('')  == 'reference_already_present')).any(),
+        'associated_gene': g['associated_gene'].unique()[0]  # assumes unique
+    }))
+    .reset_index(drop=True)
+    )
+    not_rescued = summary_df[summary_df['rescued'] == False]
+    not_rescued['mapping_hit'] = not_rescued['associated_gene'] + '_TD'
+    not_rescued = not_rescued.drop_duplicates(['rescue_candidate', 'mapping_hit'])
+    
+
+    # Mixing both we can include the counts for the transcript divergence of each gene
+    rescued = pd.concat([rescued, not_rescued])
+    rescued = rescued.drop_duplicates(['rescue_candidate', 'mapping_hit'])
+
+    not_rescued = not_rescued[['rescue_candidate', 'mapping_hit']]
+    not_rescued.columns = ['rescue_candidate', 'transcript_id']
 
     #create dictionaries of old and new counts
     old_counts = counts.set_index('transcript_id')['count'].to_dict()
@@ -71,10 +95,11 @@ def run_requant(rescue_gtf, inclusion_list, counts,
     
     #reassign counts to surviving isoforms
     rescued = rescued[rescued['rescue_candidate'].isin(old_counts.keys())]
-    inclusion_list.apply(lambda x: select_hit(x.iloc[0], rescued, new_counts, old_counts), axis = 1)
-    
+    inclusion_df = pd.concat([inclusion_df, not_rescued['transcript_id']])
+    inclusion_df.apply(lambda x: select_hit(x.iloc[0], rescued, new_counts, old_counts), axis = 1)
     #combine old and new counts
     counts_df = pd.DataFrame(rescue_gtf['transcript_id'].unique(), columns = ['transcript_id'])
+    counts_df = pd.concat([counts_df,not_rescued['transcript_id']])
     counts_df['old_count'] = counts_df['transcript_id'].apply(lambda x: fill_old_counts(x, old_counts))
     list_of_changed = []
     counts_df['new_count'] = counts_df['transcript_id'].apply(lambda x: reassign_counts(x, old_counts, new_counts, list_of_changed))
@@ -86,6 +111,9 @@ def run_requant(rescue_gtf, inclusion_list, counts,
 
     counts_df_short = counts_df[['transcript_id', 'new_count']]
     counts_df_short.to_csv(f"{prefix}_reassigned_counts.tsv", header = True, index = False, sep = '\t')
+
+    # Save transcript divergency relations
+    not_rescued.to_csv(f"{prefix}_transcript_divergency_relations.tsv", header = True, index = False, sep = '\t')
 
 def to_tpm(rescue_gtf, prefix):
     def calculate_tpm(counts, lengths):
