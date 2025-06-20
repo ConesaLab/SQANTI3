89d863f53e21b8bd40d54f6690e2e0b0135d3727
pabloati <pabloatienzalo@gmail.com>
2025-06-20 10:20:16 +0200

    Run reference and eliminated old runs

--- example/run_all_examples.sh
+++ example/run_all_examples.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # This script has to be run from the main directory of SQANTI3
 
-# Run SQANTIQC
-# sqanti3 qc -c example/config_files/qc_config.yaml
+# Run SQANTI QC
+sqanti3 qc -c example/config_files/qc_config.yaml
 # sqanti3 qc -c example/config_files/qc_config_reference.yaml
 
 # # Run SQANTI filter
-# sqanti3 filter -c example/config_files/filter_rules.yaml
-# sqanti3 filter -c example/config_files/filter_ML.yaml
+sqanti3 filter -c example/config_files/filter_rules.yaml
+sqanti3 filter -c example/config_files/filter_ML.yaml
 
 # Run SQANTI rescue
-# sqanti3 rescue -c example/config_files/rescue_automatic.yaml
-# sqanti3 rescue -c example/config_files/rescue_rules.yaml
+sqanti3 rescue -c example/config_files/rescue_automatic.yaml
+sqanti3 rescue -c example/config_files/rescue_rules.yaml
 sqanti3 rescue -c example/config_files/rescue_ml.yaml
